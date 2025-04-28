4075db5f5b079948db94941a8de2b5af0f49c635
Felipe Luz <felipetluz@outlook.com>
2025-04-28 12:05:34 -0300

    gulp corrompeu as imagens

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,11 @@ const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
 const uglify = require('gulp-uglify');
 
+function images() {
+    return gulp.src('./src/images/**/*') // pega tudo dentro da pasta images
+        .pipe(gulp.dest('./dist/images')); // copia sem alterar
+}
+
 function scripts() {
     return gulp.src('./src/scripts/*.js')
         .pipe(uglify())
@@ -15,9 +20,9 @@ function styles() {
 
 }
 
-exports.default = gulp.parallel(styles, scripts);
+exports.default = gulp.parallel(styles, scripts, images);
 
 exports.watch = function () {
     gulp.watch('./src/styles/*.scss', gulp.parallel(styles))
-    gulp.watch('./src/scripts/*js', gulp.parallel(scripts))
+    gulp.watch('./src/scripts/*.js', gulp.parallel(scripts))
 }
