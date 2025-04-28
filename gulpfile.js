3ef59a82cb464c5dd757b2a6dfcee58af019dd55
Felipe Luz <felipetluz@outlook.com>
2025-04-28 11:48:31 -0300

    retirando o imagemin por estar corrompendo as imagens no run build

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,9 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
-const imagemin = require('gulp-imagemin');
 const uglify = require('gulp-uglify');
 
 function scripts() {
-    return gulp.src('./src/scripts/*js')
+    return gulp.src('./src/scripts/*.js')
         .pipe(uglify())
         .pipe(gulp.dest('./dist/js'));
 }
@@ -16,14 +15,7 @@ function styles() {
 
 }
 
-function images() {
-    return gulp.src('./src/images/**/*')
-        .pipe(imagemin())
-        .pipe(gulp.dest('./dist/images'));
-
-}
-
-exports.default = gulp.parallel(styles, images, scripts);
+exports.default = gulp.parallel(styles, scripts);
 
 exports.watch = function () {
     gulp.watch('./src/styles/*.scss', gulp.parallel(styles))
